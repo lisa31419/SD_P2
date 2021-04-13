06e85f2a87263886a613aaed4846f5f7cf21ef87
lisa31419 <isa31419@gmail.com>
2021-04-13 11:12:51 +0200

    Artists delete finished

--- app.py
+++ app.py
@@ -54,10 +54,35 @@ class Artist(Resource):
             return {'message': "Artist with id [{}] already exists".format(id)}
 
     def delete(self, id):
-        return {'message': "Not developed yet"}, 404
+        if id is None or self.get(id) == 404:
+            return {'message': "Id must be in the list"}, 404
+        artists.pop(id)
+        return {'message': "Artist with id [{}] deleted correctly".format(id)}
 
-    def put(self, id):
-        return {'message': "Not developed yet"}, 404
+    def put(self, id=None):
+        parser = reqparse.RequestParser()  # create parameters parser from request
+
+        # define all input parameters need and its type
+
+        parser.add_argument('name', type=str, required=True, help="This field cannot be left blanck")
+        parser.add_argument('country', type=str)
+        parser.add_argument('disciplines', type=str,
+                            action="append")  # action = "append" is needed to determine that is a list of strings
+
+        data = parser.parse_args()
+
+        if id is None:
+            id = artists[len(artists) - 1]["id"] + 1
+
+        if self.get(id) == 404:
+            # new_artist
+            artists.append({'id': id,
+                            'name': data['name'],
+                            'country': data['country'],
+                            'disciplines': data['disciplines']})
+            return {'message': "Artist with id [{}] added correctly".format(id)}
+        else:
+            return {'message': "Artist with id [{}] already exists".format(id)}
 
 
 api.add_resource(Artist, '/artist/<int:id>', '/artist')
