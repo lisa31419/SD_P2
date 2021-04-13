4839c5dd5c9b292a9cd08022192f469d97a6dca3
lisa31419 <isa31419@gmail.com>
2021-04-13 13:30:26 +0200

    shows.py created

--- res/shows.py
+++ res/shows.py
@@ -6,23 +6,62 @@ from data import shows
 class Shows(Resource):
     def get(self, id):
         show = next(iter([x for x in shows if x["id"] == id]), None)
-        return {'show': show}, 200 if show else 404
+        if show is not None:
+            return {'show': show}, 200
+        else:
+            return 404
 
     def post(self, id):
-        parser = reqparse.RequestParser()  # create parameters parser from request
+        data = self.getData()
 
-        # define all input parameters need and its type
+        if id is None:
+            id = shows[len(shows) - 1]["id"] + 1
 
-        parser.add_argument('name', type=str, required=True, help="This field cannot be left blanck")
-        parser.add_argument('country', type=str)
-        parser.add_argument('disciplines', type=str,
-                            action="append")  # action = "append" is needed to determine that is a list of strings
+        if self.get(id) == 404:
+            # new_show
+            shows.append({'id': id,
+                          'name': data['name'],
+                          'date': data['date'],
+                          'place': data['place'],
+                          'artist': data['artist']})
+            return {'message': "Show with id [{}] added correctly".format(id)}
+        else:
+            return {'message': "Show with id [{}] already exists".format(id)}
 
-        data = parser.parse_args()
-        return {'message': "Not developed yet"}, 404
 
-    def delete(self, id):
-        return {'message': "Not developed yet"}, 404
+def delete(self, id):
+    if id is None or self.get(id) == 404:
+        return {'message': "Id must be in the list"}, 404
+    shows.pop(id)
+    return {'message': "Show with id [{}] deleted correctly".format(id)}
 
-    def put(self, id):
-        return {'message': "Not developed yet"}, 404
+
+def put(self, id):
+    data = self.getData()
+
+    if self.get(id) == 404:
+        self.post(id)
+        return {'message': "Show with id [{}] will be created".format(id)}
+    else:
+        shows[id] = {'id': id,
+                     'name': data['name'],
+                     'date': data['date'],
+                     'place': data['place'],
+                     'artist': data['artist']}
+        return {'message': "Show with id [{}] updated".format(id)}
+
+
+def getData(self):
+    parser = reqparse.RequestParser()  # create parameters parser from request
+
+    # define all input parameters need and its type
+
+    parser.add_argument('name', type=str, required=True, help="This field cannot be left blanck")
+    parser.add_argument('date', type=str)
+    parser.add_argument('place', type=str,
+                        action="append")  # action = "append" is needed to determine that is a list of strings
+    parser.add_argument('artist', type=str,
+                        action="append")  # action = "append" is needed to determine that is a list of strings
+
+    data = parser.parse_args()
+    return data
