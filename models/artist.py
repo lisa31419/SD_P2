ecea6b2bdf68cad09dd8bad5aeb871696fc19bb6
lisa31419 <isa31419@gmail.com>
2021-04-18 20:56:45 +0200

    All works till exercise 3 (included).

--- models/artist.py
+++ models/artist.py
@@ -5,6 +5,7 @@ disciplines = ('THEATRE', 'MUSIC', 'DANCE', 'CIRCUS', 'OTHER')
 
 class ArtistModel(db.Model):
     __tablename__ = 'artists'  # This is table name
+    __table_args__ = (db.UniqueConstraint('name', 'country', 'discipline'),)  # Extracted comma
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(30), unique=True, nullable=False)
