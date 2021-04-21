8e912b9d6c08a4148a55050c06cd944a9055f7df
lisa31419 <isa31419@gmail.com>
2021-04-21 20:02:41 +0200

    Ex5 part 4 done, except disciplines

--- res/shows.py
+++ res/shows.py
@@ -88,26 +88,26 @@ class ShowArtistsList(Resource):
 class ShowArtist(Resource):
     def get(self, id_show, id_artist):
         artists_in_show = ShowModel.find_by_id(id_show).artists
-        artist_found = [x.json() for x in artists_in_show if x.id == id_artist]
+        artist_found = [x.json() for x in artists_in_show if str(x.id) == id_artist]
         if artist_found:
             return {'artist': artist_found}, 200
         else:
             return {"message": "There are no artists with id [{}] in this show.".format(id_artist)}, 404
 
     def post(self, id_show, id_artist=None):
-        #data = Artist.getData(self)
+        # data = Artist.getData(self)
         show_found = ShowModel.find_by_id(id_show)
         artists_in_show = show_found.artists
         if id_artist is None:
             id_artist = ArtistModel.length() + 1
 
         if ArtistModel.find_by_id(id_artist) is None:
-            #new_artist = ArtistModel(data['name'], data['country'], data['disciplines'])
+            # new_artist = ArtistModel(data['name'], data['country'], data['disciplines'])
             try:
-                #new_artist.save_to_db()
-                #artists_in_show.append(new_artist)
-                #db.session.commit()
-                return {'message': "Artist with id [{}] created and added correctly to the show.".format(id_artist)}
+                # new_artist.save_to_db()
+                # artists_in_show.append(new_artist)
+                # show_found.save_to_db()
+                return {'message': "Artist with id [{}] created and added correctly to the show.".format(id_artist)}, 200
             except:
                 return {"message": "An error occurred inserting the new artist into the show."}, 500
 
@@ -116,11 +116,18 @@ class ShowArtist(Resource):
             try:
                 artists_in_show.append(artist_found)
                 show_found.save_to_db()
-                return {'message': "Artist with id [{}] added correctly to the show.".format(id_artist)}
+                return {'message': "Artist with id [{}] added correctly to the show.".format(id_artist)}, 200
             except:
                 return {"message": "An error occurred inserting the artist into the show."}, 500
 
-
-'''
     def delete(self, id_show, id_artist):
-'''
+        show_found = ShowModel.find_by_id(id_show)
+        artists_in_show = show_found.artists
+        artist_to_delete = [x for x in artists_in_show if str(x.id) == id_artist]
+        if artist_to_delete:
+            artist_to_delete = ArtistModel.find_by_id(id_artist)
+            artists_in_show.remove(artist_to_delete)
+            show_found.delete_from_db()
+            return {'message': "Artist with id [{}] deleted correctly from the show".format(id_artist)}, 200
+        else:
+            return {'message': "Artist must be in the show"}, 404
