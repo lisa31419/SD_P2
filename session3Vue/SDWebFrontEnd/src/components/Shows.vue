9eac939ba96cbfb26f40a24f849bf7cf25e0bf87
lisa31419 <isa31419@gmail.com>
2021-05-05 03:26:23 +0200

    fixing bugs

--- session3Vue/SDWebFrontEnd/src/components/Shows.vue
+++ session3Vue/SDWebFrontEnd/src/components/Shows.vue
@@ -1,53 +1,65 @@
 <template>
   <div id="app">
     <body style="background-color:lightgrey;">
-    <div v-if="isShowingCart && shows_added.length > 0"  class...>
-      <!-- Displaying in case button go to Cart True-->
-      <div class="container center-h center-v">
-        <div class="row">
-          <div class="col-sm-10">
-            <h1 class="card-header text-center"><b>Cart</b></h1>
-            <hr>
-            <br>
-            <table class="table table-hover">
-              <thead>
-              <tr>
-                <th scope="col">Event Name</th>
-                <th scope="col">Quantity</th>
-                <th scope="col">Total Price</th>
-                <th scope="col">Total Tickets</th>
-                <th scope="col"></th>
-                <th></th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr v-for="(show) in shows_added" :key="show.id">
+    <!-- Displaying in case button go to Cart True-->
+    <div v-if="isShowingCart"  class...>
+      <!-- Show nothing id cart is empty-->
+      <div v-if="shows_added.length > 0">
+        <div class="container center-h center-v">
+          <div class="row">
+            <div class="col-sm-10">
+              <h1 class="card-header text-center"><b>Cart</b></h1>
+              <hr>
+              <br>
+              <table class="table table-hover">
+                <thead>
+                <tr>
+                  <th scope="col">Event Name</th>
+                  <th scope="col">Quantity</th>
+                  <th scope="col">Total Price</th>
+                  <th scope="col">Total Tickets</th>
+                  <th scope="col"></th>
+                  <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="(show) in shows_added" :key="show.id">
                   <td>{{ show['show'].name }}</td>
                   <td>{{show['quantity']}}
                     <div class="btn-group" role="group">
-                      <button class="btn btn-danger btn-lg mr-1" @click="returnTickets(show)"> -</button>
-                      <button class="btn btn-success btn-lg" @click="buyTickets(show)"> +</button>
+                      <button class="btn btn-danger btn-lg mr-1" @click="returnTickets(show)">-</button>
+                      <button class="btn btn-success btn-lg" @click="buyTickets(show)">+</button>
                     </div>
                   </td>
                   <td>{{ show['show'].price }}</td>
                   <td>{{show['quantity']*show['show'].price}}</td>
                   <td>
-                    <div class="btn-group" role="group">
-                      <button class="btn btn-danger btn-md" type="button" @click="deleteEventFromCart(show)">Delete Ticket</button>
-                    </div>
+                    <button class="btn btn-danger btn-md" @click="deleteEventFromCart(show)">Delete Ticket</button>
                   </td>
                 </tr>
-              </tbody>
-            </table>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="card-footer" style="text-align: center;">
+          <button class="btn btn-danger btn-lg down-left" @click="goToShows()"> Back</button>
+          <button class="btn btn-success btn-lg down-right" @click="finalizePurchase()"> Finalize Purchase</button>
+        </div>
+      </div>
+      <!-- Show the cart if it is not empty-->
+      <div v-else class...>
+        <div class="container">
+          <h1 class="text-center"><b>Cart</b></h1>
+          <h4 class="text-center"> Your cart is currently empty.</h4>
+          <br>
+          <div class="card-footer" style="text-align: center;">
+            <button class="btn btn-danger btn-lg down-left" @click="goToShows()"> Back</button>
+            <button class="btn btn-success btn-lg down-right" disabled> Finalize Purchase</button>
           </div>
         </div>
       </div>
-      <div class="card-footer" style="text-align: center;">
-        <button class="btn btn-danger btn-lg down-left" @click="goToShows()"> Back</button>
-        <button class="btn btn-success btn-lg down-right"> Finalize Purchase</button>
-      </div>
     </div>
-    <div v-else-if="shows_added.length === 0">Your cart is currently empty.</div>
     <!-- Show the shows when button is deactivated-->
     <div v-else class...>
       <button class='btn btn-success pull-left' @click="goToCart()"> Cart</button>
@@ -74,7 +86,10 @@
             <div class="card text-white bg-dark mb-3 text-center" style="max-width: 18rem;">
               <div class="card-body">
                 <h4> Tickets available: {{ tickets_available }} </h4>
-                <button class="btn btn-success btn-lg" @click="addEventToCart(show); this.disabled=true;"> Add show to cart</button>
+                <button class="btn btn-success btn-lg" @click="addEventToCart(show)" :disabled="just_shows.includes(show)"> Add show to cart </button>
+                <h6>{{ just_shows.includes(show) }}</h6>
+                <h6>{{ just_shows }}</h6>
+                <h6>{{ just_shows.indexOf(show) }}</h6>
               </div>
             </div>
           </div>
@@ -98,7 +113,7 @@ export default {
       message: 'My first component',
       tickets_bought: 0,
       price_event: 10,
-      money_available: 100000000,
+      money_available: 200,
       shows: [
         {
           name: 'Festival Cruilla 2020',
@@ -153,17 +168,17 @@ export default {
         }
       ],
       shows_added: [],
+      just_shows: [],
+      index: 0,
       isShowingCart: false
     }
   },
   methods: {
     goToCart () {
-      // this.$router.push({name: 'CartPage'})
       this.isShowingCart = true
     },
     goToShows () {
       this.isShowingCart = false
-      this.shows_added = []
     },
     buyTickets (tickets) {
       if (this.money_available >= tickets['show'].price) {
@@ -178,11 +193,41 @@ export default {
       }
     },
     addEventToCart (show) {
-      this.shows_added.push({'show': show, 'quantity': 0})
+      if (!this.shows_added.includes({'show': show, 'quantity': 0})) {
+        this.shows_added.push({'show': show, 'quantity': 0})
+        this.just_shows.push(show)
+      }
     },
     deleteEventFromCart (show) {
-      this.shows_added.remove(show)
-      // Arreglar esto
+      this.index = this.just_shows.indexOf(show)
+      this.shows_added = this.shows_added.filter(x => {
+        return x !== show.name
+      })
+      this.just_shows = this.just_shows.filter(x => {
+        return x.id_show !== show.id_show
+      })
+      // Funciona a medias, resetea tot
+    },
+    addPurchase (parameters) {
+      const path = 'http://localhost:5000/order/test'
+      axios.post(path, parameters)
+        .then(() => {
+          console.log('Order done')
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log(error)
+          this.getShows()
+        })
+    },
+    finalizePurchase () {
+      for (let i = 0; i < this.shows_added.items.length; i += 1) {
+        const parameters = {
+          id_show: this.shows_added.items[i].show.id,
+          tickets_bought: this.shows_added.items[i].quantity
+        }
+        this.addPurchase(parameters)
+      }
     },
     getShows () {
       const path = 'http://localhost:5000/shows'
