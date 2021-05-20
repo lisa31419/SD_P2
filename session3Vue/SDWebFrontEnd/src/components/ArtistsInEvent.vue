7f9fad32726a7ed06160fa0286402f19aa3d972a
lisa31419 <isa31419@gmail.com>
2021-05-20 17:33:41 +0200

    S6 checkbox de disciplinas

--- session3Vue/SDWebFrontEnd/src/components/ArtistsInEvent.vue
+++ session3Vue/SDWebFrontEnd/src/components/ArtistsInEvent.vue
@@ -20,9 +20,27 @@
           </div>
           <div class="form-label-group">
             <br>
-            <label for="inputGenre">Artist's Genre</label>
-            <input type="text" id="inputGenre" class="form-control"
-                   placeholder="Enter artist's genre" required v-model="addArtistForm.city">
+            <label>Artist's Genre</label>
+            <label class="container">Theatre
+              <input type="checkbox" id="THEATRE" @click="addDisciplines('THEATRE')">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Music
+              <input type="checkbox" id="MUSIC" @click="addDisciplines('MUSIC')">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Dance
+              <input type="checkbox" id="DANCE" @click="addDisciplines('DANCE')">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Circus
+              <input type="checkbox" id="CIRCUS" @click="addDisciplines('CIRCUS')">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Other
+              <input type="checkbox" id="OTHER" @click="addDisciplines('OTHER')">
+              <span class="checkmark"></span>
+            </label>
           </div>
           <br>
           <div class="d-flex justify-content-center" >
@@ -54,7 +72,7 @@ export default {
         id: '',
         name: '',
         country: '',
-        genre: ''
+        disciplines: []
       }
     }
   },
@@ -66,14 +84,30 @@ export default {
       this.addArtistForm.id = ''
       this.addArtistForm.name = ''
       this.addArtistForm.country = ''
-      this.addArtistForm.genre = ''
+      this.addArtistForm.disciplines = []
+    },
+    addDisciplines (disciplina) {
+      let checkbox = document.getElementById(disciplina)
+      if (checkbox.checked) {
+        if (!this.addArtistForm.disciplines.includes(disciplina)) {
+          this.addArtistForm.disciplines.push(disciplina)
+        }
+      } else {
+        this.deleteDisciplines(disciplina)
+      }
+    },
+    deleteDisciplines (disciplina) {
+      if (this.addArtistForm.disciplines.includes(disciplina)) {
+        let index = this.addArtistForm.disciplines.indexOf(disciplina)
+        this.addArtistForm.disciplines.splice(index, 1)
+      }
     },
     onSubmitAddArtistInShow (evt) {
       evt.preventDefault()
       const parameters = {
         name: this.addArtistForm.name,
         country: this.addArtistForm.country,
-        genre: this.addArtistForm.genre
+        disciplines: this.addArtistForm.disciplines
       }
       this.addNewArtist(parameters)
     },
@@ -88,7 +122,7 @@ export default {
           id: this.addArtistForm.id,
           name: this.addArtistForm.name,
           country: this.addArtistForm.country,
-          genre: this.addArtistForm.genre
+          disciplines: this.addArtistForm.disciplines
         }
         console.log(params)
         this.addArtistInShow(params)
@@ -99,7 +133,7 @@ export default {
             id: this.addArtistForm.id,
             name: this.addArtistForm.name,
             country: this.addArtistForm.country,
-            genre: this.addArtistForm.genre
+            disciplines: this.addArtistForm.disciplines
           }
           this.addArtistInShow(params)
           console.error(error)
@@ -180,5 +214,72 @@ body  {
   background-color: #3178D6;
   background-image: linear-gradient(#3178D6, #205FB2);
   color: whitesmoke;
+}
+/* The container */
+.container {
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 16px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default checkbox */
+.container input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25px;
+  width: 25px;
+  background-color: #eee;
+}
+
+/* On mouse-over, add a grey background color */
+.container:hover input ~ .checkmark {
+  background-color: #ccc;
+}
+
+/* When the checkbox is checked, add a blue background */
+.container input:checked ~ .checkmark {
+  background-color: #2196F3;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.container input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the checkmark/indicator */
+.container .checkmark:after {
+  left: 9px;
+  top: 5px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
 }
 </style>
